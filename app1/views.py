3794263bfc406750a741b830e175b10bcd9bddd4
indieka900 <indiekaj@gmail.com>
2023-03-03 12:23:23 +0300

    Done

--- app1/views.py
+++ app1/views.py
@@ -7,8 +7,9 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 #from . import forms
+from django.views.decorators.csrf import csrf_exempt
 
-
+@csrf_exempt
 def log_in(request):
     if request.user.is_authenticated:
         return redirect('home')
